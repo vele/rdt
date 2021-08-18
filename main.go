9317549c97d83ff752681c01cb053bdd3f4e0413
vele <m.hristov@executie.org>
2021-08-18 01:22:42 +0100

    commit

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
 	"github.com/vele/rdt/vsphere"
+	"github.com/vmware/govmomi/simulator"
+	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/mo"
 )
 
 const (
@@ -16,7 +22,6 @@ const (
 	envInsecure = "GOVMOMI_INSECURE"
 )
 
-var v = vsphere.NewClient()
 var urlDescription = fmt.Sprintf("ESX or vCenter URL [%s]", envURL)
 var urlFlag = flag.String("url", getEnvString(envURL, ""), urlDescription)
 
@@ -46,5 +51,48 @@ func getEnvBool(v string, def bool) bool {
 	return false
 }
 func main() {
+	flag.Parse()
+	Run(func(ctx context.Context, c *vim25.Client) error {
+		// Create a view of Network types
+		m := view.NewManager(c)
 
+		v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"Network"}, true)
+		if err != nil {
+			return err
+		}
+
+		defer v.Destroy(ctx)
+
+		// Reference: http://pubs.vmware.com/vsphere-60/topic/com.vmware.wssdk.apiref.doc/vim.Network.html
+		var networks []mo.Network
+		err = v.Retrieve(ctx, []string{"Network"}, nil, &networks)
+		if err != nil {
+			return err
+		}
+
+		for _, net := range networks {
+			fmt.Printf("%s: %s\n", net.Name, net.Reference())
+		}
+
+		return nil
+	})
+}
+func Run(f func(ctx context.Context, c *vim25.Client) error) {
+	var err error
+	var c *vim25.Client
+	parsedOptions := &vsphere.VsphereClientOptions{}
+	parsedOptions.Url = *urlFlag
+	parsedOptions.Insecure = *insecureFlag
+	if *urlFlag == "" {
+		err = simulator.VPX().Run(f)
+	} else {
+		ctx := context.Background()
+		c, err = vsphere.NewClient(ctx, parsedOptions)
+		if err == nil {
+			err = f(ctx, c)
+		}
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- vsphere/vim.go
+++ vsphere/vim.go
@@ -2,28 +2,26 @@ package vsphere
 
 import (
 	"context"
-	"log"
 
 	"github.com/vmware/govmomi/session/cache"
-	"github.com/vmware/govmomi/simulator"
 	"github.com/vmware/govmomi/vim25"
 	"github.com/vmware/govmomi/vim25/soap"
 )
 
 type VsphereClientOptions struct {
-	urlFlag      string
-	insecureFlag bool
+	Url      string
+	Insecure bool
 }
 
-func (v VsphereClientOptions) NewClient(ctx context.Context) (*vim25.Client, error) {
-	u, err := soap.ParseURL(v.urlFlag)
+func NewClient(ctx context.Context, v *VsphereClientOptions) (*vim25.Client, error) {
+	u, err := soap.ParseURL(v.Url)
 	if err != nil {
 		return nil, err
 	}
 	processOverride(u)
 	s := &cache.Session{
 		URL:      u,
-		Insecure: v.insecureFlag,
+		Insecure: v.Insecure,
 	}
 	c := new(vim25.Client)
 	err = s.Login(ctx, c, nil)
@@ -31,22 +29,5 @@ func (v VsphereClientOptions) NewClient(ctx context.Context) (*vim25.Client, err
 		return nil, err
 	}
 	return c, nil
-}
-func (v VsphereClientOptions) Run(f func(context.Context, *vim25.Client) error) {
 
-	var err error
-	var c *vim25.Client
-
-	if v.urlFlag == "" {
-		err = simulator.VPX().Run(f)
-	} else {
-		ctx := context.Background()
-		c, err = v.NewClient(ctx)
-		if err == nil {
-			err = f(ctx, c)
-		}
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
 }
